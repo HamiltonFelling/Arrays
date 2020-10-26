afd45fe5baa4233b241834dfb3e85302c50b805f
HamiltonFelling <832419@mystma.org>
2020-10-26 20:02:00 +0000

    Progress

---
+++ Arrays/src/app/App.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        String sen[] = {"Rogan","Brandon"};
+        String jun[] = {"Bryan", "Hamilton", "Kenadi", "Mason",};
+        String sop[] = {"Luke", "Will", "Alexis", "Taten"};
+
+        System.out.println("Please type a student name to know their grade level");
+
+        String student = input.next.Line();
+
+        for (int i = 0; i < sop.lenght; i++){
+            if(student.equal(sop[i]))
+            
+        }
+        
+    }
+}
